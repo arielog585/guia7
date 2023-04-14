132dc2d50a973509b85a275a2f17f5430f80e1f1
Ariel Gayone <arielgayone@gmail.com>
2023-04-14 20:06:39 -0300

    prueba net

---
+++ src/mascotas_entidades/Mascota.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mascotas_entidades;
+
+/**
+ *
+ * @author Maquina
+ */
+public class Mascota {
+    public String nombre;
+    public String apodo;
+    public String tipo;
+    public String color;
+    public String raza;
+    public int edad;
+    public boolean cola;
+}
